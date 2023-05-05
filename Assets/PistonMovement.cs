a81a7fdd182db9b87a56ed7cbddb2e4f0971a19d
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-05-05 15:16:45 -0700

    Updated Piston functionality
    Now piston will always stay on screen.

--- Assets/PistonMovement.cs
+++ Assets/PistonMovement.cs
@@ -6,25 +6,47 @@ public class PistonMovement : MonoBehaviour
 {
     private GameObject finishLine = null;
     private GameObject wall = null;
-    private GameObject rod = null;
     private float finishX;
-    
+    //Camera edge
+    private Camera mainCamera;
+    private float leftEdge;
+
+
     public float pistonSpeed = 10f; //Change this variable to speed or slow piston
     // Start is called before the first frame update
     void Start()
     {
         wall = GameObject.Find("Piston");
-        //rod = GameObject.Find("Rod");
         finishLine = GameObject.FindGameObjectWithTag("Finish");
         finishX = finishLine.transform.position.x;
+
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (wall.transform.position.x < finishX){
-            wall.transform.position = new Vector3(wall.transform.position.x + (pistonSpeed / 500), wall.transform.position.y, 0);
-            //rod.transform.position = new Vector3(rod.transform.position.x + (pistonSpeed / 500), rod.transform.position.y, 0);
+        CalculateLeftEdge();
+        if (wall.transform.position.x < finishX)
+        {
+            if (wall.transform.position.x > leftEdge - .001)
+            {
+                wall.transform.position = new Vector3(wall.transform.position.x + (pistonSpeed / 500), wall.transform.position.y, 0);
+
+            }
+            else
+            {
+                wall.transform.position = new Vector3(leftEdge, wall.transform.position.y, 0);
+            }
         }
+
+    }
+
+    private void CalculateLeftEdge()
+    {
+        float distanceFromCamera = Mathf.Abs(transform.position.z - mainCamera.transform.position.z);
+        Vector3 leftViewportPoint = new Vector3(0f, 0.5f, distanceFromCamera);
+        Vector3 leftWorldPoint = mainCamera.ViewportToWorldPoint(leftViewportPoint);
+        leftEdge = leftWorldPoint.x;
     }
 }
